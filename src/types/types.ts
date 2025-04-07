4a8483a2322fc5e2e530443e18271993c89e87d3
jinsoul <wlsthf75@zetalux.co.kr>
2025-04-07 14:52:04 +0900

    chore: 컴포넌트 모듈화

---
+++ src/types/types.ts
@@ -0,0 +1,29 @@
+export interface Item {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface Notification {
+  id: number;
+  message: string;
+  type: "info" | "success" | "warning" | "error";
+}
+
+export interface AppContextType {
+  theme: string;
+  toggleTheme: () => void;
+  user: User | null;
+  login: (email: string, password: string) => void;
+  logout: () => void;
+  notifications: Notification[];
+  addNotification: (message: string, type: Notification["type"]) => void;
+  removeNotification: (id: number) => void;
+}
