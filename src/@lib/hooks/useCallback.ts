ab95b3155b817e44cd6d8f7710e930fcc6ecb024
jinsoul <wlsthf75@zetalux.co.kr>
2025-04-07 13:07:26 +0900

    feat: useCallback 로직 구현

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
 import { useMemo } from "./useMemo";
+import { shallowEquals } from "../equalities";
 
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList,
 ) {
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return useMemo(() => factory, _deps);
+
+  // useMemo를 그대로 쓰면 React의 ESLint 규칙이 적용된다.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(() => factory, _deps, shallowEquals);
 }
