e76d0a163f93e76a7a5e2e04cf12b1cdafd545e5
jinsoul <wlsthf75@zetalux.co.kr>
2025-04-11 08:35:00 +0900

    chroe: Lazy initialization 적용

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -2,6 +2,6 @@ import { useState } from "react";
 
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
-  const [value] = useState({ current: initialValue });
+  const [value] = useState(() => ({ current: initialValue }));
   return value;
 }
