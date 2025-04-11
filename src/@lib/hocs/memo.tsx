b194f22a3c43c67210b2c355f4e61b0f84e82c23
jinsoul <wlsthf75@zetalux.co.kr>
2025-04-11 09:40:47 +0900

    chore: useRef import 변경

--- src/@lib/hocs/memo.tsx
+++ src/@lib/hocs/memo.tsx
@@ -1,5 +1,6 @@
 import { shallowEquals } from "../equalities";
-import { ComponentType, useRef } from "react";
+import { ComponentType } from "react";
+import { useRef } from "../hooks";
 
 // memo HOC는 컴포넌트의 props를 얕은 비교하여 불필요한 리렌더링을 방지합니다.
 export function memo<P extends object>(
